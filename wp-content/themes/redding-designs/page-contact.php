c952711ff8b618206a8aed9dfbd02a824d9f90d9
Iseeumhmm <rickheffren@Ricks-MacBook-Pro.local>
2019-10-28 11:22:35 -0400

    price estimator

--- wp-content/themes/redding-designs/page-contact.php
+++ wp-content/themes/redding-designs/page-contact.php
@@ -23,28 +23,38 @@ get_header();
                 </div>
                 <div class="container-white">
                     <section class="meet-the-team">
-                            <div class="flex">
-                            <?php 
-                            $portrait_styles = array();
-                            $wpb_all_query = new WP_Query(array('post_type'=>'contact', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
-                            <?php if ( $wpb_all_query->have_posts() ) : while ( $wpb_all_query->have_posts() ) : ?>
-                                <?php $wpb_all_query->the_post(); ?>
-                                    <div class="cell-service">
-                                        <div class="menu-icon--container">
-                                            <?php echo bulletPoints( get_field('icon') )['html'];?>
-                                        </div>
-                                        <div class="card-body">
-                                            <h2><?php the_title(); ?></h2>
-                                            <p><?php echo get_field( 'description' ); ?></p>
-                                        </div>
+                        <div class="flex">
+                        <?php 
+                        $portrait_styles = array();
+                        $wpb_all_query = new WP_Query(array('post_type'=>'contact', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
+                        <?php if ( $wpb_all_query->have_posts() ) : while ( $wpb_all_query->have_posts() ) : ?>
+                            <?php $wpb_all_query->the_post(); ?>
+                                <div class="cell-service" data-aos="zoom-in">
+                                    <div class="menu-icon--container">
+                                        <?php echo bulletPoints( get_field('icon') )['html'];?>
                                     </div>
-                                    <?php $i++; ?>
-                                    <?php endwhile; ?>
-                                    <?php wp_reset_postdata(); ?>
-                                    <?php else : ?>
-                                        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-                                    <?php endif; ?>
+                                    <div class="card-body">
+                                        <h2><?php the_title(); ?></h2>
+                                        <p><?php echo get_field( 'description' ); ?></p>
+                                    </div>
+                                </div>
+                                <?php $i++; ?>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
+                                <?php else : ?>
+                                    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+                                <?php endif; ?>
+                        </div>
+                        <div class="container-grey">
+                            <section data-aos="zoom-in">
+                                <h3 class="heading">Get a FREE Custom Quote to Suit Your Needs</h3>
+                                <p>Use our fun and super friendly online estimator app to send us a detailed description of your website needs.</p>
+                                <p class="btn"><a href="http://beardreview.com/priceestimator/hidden/" target="_blank">get a started!</a></p>
+                            </section>
+                            <div class="latest">                
+                                <div class="price-estimator" data-aos="slide-right"></div>
                             </div>
+                        </div>
                     </section>
                 </div>
             </div>
