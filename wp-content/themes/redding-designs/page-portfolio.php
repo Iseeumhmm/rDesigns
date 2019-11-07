e8b900c4dd3fae7ad6d6981d25c390bdfb23a043
Iseeumhmm <rickheffren@Ricks-MacBook-Pro.local>
2019-11-07 13:52:03 -0500

    Changes requested

--- wp-content/themes/redding-designs/page-portfolio.php
+++ wp-content/themes/redding-designs/page-portfolio.php
@@ -79,43 +79,70 @@ get_header();
                         <div class="phone" data-aos="slide-right"></div>
                     </div>
                 </div>
+                
                 <div class="container-white container-white--services">
                     <section>
                         <div class="flex">
-                        <?php 
-                            // Get Services from database
-                            $wpb_all_query = new WP_Query(array('post_type'=>'portfolio', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
-                            <?php if ( $wpb_all_query->have_posts() ) : while ( $wpb_all_query->have_posts() ) : ?>
-                                <?php $wpb_all_query->the_post(); ?>
-                                <div class="cell-service" data-aos="zoom-in"> 
+                        <?php
+                            $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+                            $args = array(
+                                'post_type' => 'portfolio',
+                                'posts_per_page' => 6,
+                                'paged' => $paged
+                            );
+                            $loop = new WP_Query( $args );
+                            while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                            <div class="cell-service" data-aos="zoom-in"> 
                                 <div class="portfolio-main_image" style="background-image: url('<?php the_field( 'image' )?>')"></div>
-                                <?php
-                                echo '<div class="card-body">';
-                                echo '<h2>' . get_the_title() . '</h2>';
-                                echo '<p>' . get_field( "sub_heading" ) . '</p>';
-                                ?>
-                                <?php
-                                    $table = get_field( 'bullet_points' );
-                                    if ( ! empty ( $table ) ) {
-                                        foreach ( $table['body'] as $tr ) {
-                                            foreach ( $tr as $td ) {
-                                                $bullets = bulletPoints( $td['c'] );
-                                                echo $bullets['html'];
-                                                echo '<p class="card-text">' . $bullets['label'] . '</p><br>';
+                                    <?php
+                                    echo '<div class="card-body">';
+                                    echo '<h2>' . get_the_title() . '</h2>';
+                                    echo '<p>' . get_field( "sub_heading" ) . '</p>';
+                                    ?>
+                                    <?php
+                                        $table = get_field( 'bullet_points' );
+                                        if ( ! empty ( $table ) ) {
+                                            foreach ( $table['body'] as $tr ) {
+                                                foreach ( $tr as $td ) {
+                                                    $bullets = bulletPoints( $td['c'] );
+                                                    echo $bullets['html'];
+                                                    echo '<p class="card-text">' . $bullets['label'] . '</p><br>';
+                                                }
                                             }
-                                        }
-                                    } 
-                                    echo '<p class="btn"><a href="' . get_field( 'website' ) . '">visit website</a></p>';
-                                    echo '</div>';
+                                        } 
+                                        echo '<p class="btn"><a href="' . get_field( 'website' ) . '">visit website</a></p>';
+                                        echo '</div>';
                                     ?>
                             </div>
                             <?php endwhile; ?>
+                            <div class="pagination">
+                                <?php
+                                $big = 999999999;
+                                echo paginate_links( array(
+                                    'base' => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
+                                    'format' => '?paged=%#%',
+                                    'current' => max( 1, get_query_var('paged') ),
+                                    'total' => $loop->max_num_pages,
+                                    'prev_text' => '&laquo;',
+                                    'next_text' => '&raquo;'
+                                ) );
+                            ?>
+                            </div>
                             <?php wp_reset_postdata(); ?>
-                            <?php else : ?>
-                                <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-                            <?php endif; ?>
                         </div>
                     </section>
+                </div>
+                <div class="container-grey">
+                    <div class="price-estimator--container">
+                        <section data-aos="zoom-in">
+                            <h3 class="heading">Get a FREE Custom Quote to Suit Your Needs</h3>
+                            <p>Use our fun and super friendly online estimator app to send us a detailed description of your website needs.</p>
+                            <p class="btn"><a href="http://beardreview.com/priceestimator/hidden/" target="_blank">get started!</a></p>
+                        </section>
+                        <div class="latest">                
+                            <div class="price-estimator" data-aos="slide-right"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
 		</main><!-- #main -->
