d89c85a5be6ab71a22d1c59ad753ef108ae8b911
Frank Wiebe <frank@reddingdesigns.com>
2020-03-20 16:36:28 -0400

    Adding image to portfolio pages

--- wp-content/themes/redding-designs/page-portfolio.php
+++ wp-content/themes/redding-designs/page-portfolio.php
@@ -30,7 +30,7 @@ get_header();
             </style>
             <div class="portfolio">
                 <div class="header-spacer"></div>
-                <div class="sub-banner banner-portfolio" data-aos="fade-in">
+                <div class="sub-banner banner-portfolio" style="background-image: url('<?php the_field( "main_image" ); ?>');" data-aos="fade-in">
                     <div class="sub-banner--text_container">
                         <h1 data-aos="slide-right">Our Portfolio</h1><br>
                     </div>
